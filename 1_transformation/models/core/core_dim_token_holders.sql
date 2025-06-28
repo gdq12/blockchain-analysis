b83767b64a10e04f1a543c4d832a247fd599b7f8
gdq12 <gdquiceno@gmail.com>
2025-06-28 18:58:07 +0200

    flexible snapshot date implementation to be deteremined by defined var in dbt run command

--- 1_transformation/models/core/core_dim_token_holders.sql
+++ 1_transformation/models/core/core_dim_token_holders.sql
@@ -1,3 +1,5 @@
+{% set snapshot_granularity = var('snapshot_granularity', 'MONTH') %}
+
 with base_trnfs as 
 (select 
   address token_address,
@@ -19,7 +21,7 @@ snap_shots as
 (select 
   token_address,
   wallet_address,
-  date_trunc(block_timestamp, MONTH) snapshot_date,
+  date_trunc(block_timestamp, {{ snapshot_granularity | upper }} ) snapshot_date,
   min(block_number) first_blck_num, 
   max(block_number) last_blck_num,
   sum(balance) balance
@@ -27,18 +29,19 @@ from base_trnfs
 group by all 
 )
 select 
-  ss.token_address, 
-  dtk.token_symbol,
-  dtk.token_name,
-  dtk.token_decimals,
-  ss.wallet_address,
-  dw.wallet_type,
-  ss.snapshot_date,
-  ss.first_blck_num, 
-  ss.last_blck_num,
-  sum(ss.balance) over (partition by ss.token_address, ss.wallet_address 
+    ss.token_address, 
+    dtk.token_symbol,
+    dtk.token_name,
+    dtk.token_decimals,
+    ss.wallet_address,
+    dw.wallet_type,
+    ss.snapshot_date,
+    ss.first_blck_num, 
+    ss.last_blck_num,
+    sum(ss.balance) over (partition by ss.token_address, ss.wallet_address 
                     order by ss.snapshot_date
-                    rows between unbounded preceding and current row) balance
+                    rows between unbounded preceding and current row) balance,
+    {{ dbt.current_timestamp() }} transformation_dt
 from snap_shots ss 
 left join {{ ref('core_dim_tokens') }} dtk on ss.token_address = dtk.token_address
 left join {{ ref('core_dim_wallets') }} dw on ss.wallet_address = dw.address
